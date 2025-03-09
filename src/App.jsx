b6f2798c727eba8f17cb78181fc507edd99c533b
andrey <tvrgb3677@gmail.com>
2025-03-09 14:29:21 +0800

    new html

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import { Routes, Route, Link } from "react-router-dom";
 import StrOne from "./pages/1str";
 import StrTwo from "./pages/2str";
 import StrThree from "./pages/3str";
+
 const objectLinks = [
   {
     path: "/",
-    jsxElement: <StrOne/>,
+    jsxElement: <StrOne />,
   },
   {
     path: "normal",
-    jsxElement: <StrTwo/>,
+    jsxElement: <StrTwo />,
   },
   {
     path: "hard",
-    jsxElement: <StrThree/>,
+    jsxElement: <StrThree />,
   },
 ];
+
 function App() {
   return (
-    <>
-<Routes>
-  <Route path="/" element={<Layout/>}>
-  {objectLinks.map((objectLink) => (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {objectLinks.map((objectLink, index) => (
           <Route
-         
+            key={index} // Добавьте уникальный ключ
             path={objectLink.path}
             element={objectLink.jsxElement}
           />
-        ))}</Route>
-</Routes>
-    </>
+        ))}
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet, NavLink } from "react-router-dom";
-import './Layout.css'
+import "./Layout.css";
+
 function getNavLinkClass({ isActive }) {
   return isActive ? "url active-link" : "url";
 }
+
 const navLinkObjects = [
   {
     to: "/",
@@ -13,20 +15,22 @@ const navLinkObjects = [
     content: "Normal",
   },
   {
-    to: "hard",
+    to: "/hard", // Добавлен "/" в начало пути
     content: "Hard",
   },
 ];
+
 export default function Layout() {
   return (
     <>
       <header>
-        {navLinkObjects.map((navLinkObject) => (
-          <NavLink to={navLinkObject.to} className={getNavLinkClass}>
-            <button className="button-url">
-            {navLinkObject.content}
-            </button>
-           
+        {navLinkObjects.map((navLinkObject, index) => (
+          <NavLink
+            key={index} // Добавьте уникальный ключ
+            to={navLinkObject.to}
+            className={getNavLinkClass}
+          >
+            <button className="button-url">{navLinkObject.content}</button>
           </NavLink>
         ))}
       </header>
